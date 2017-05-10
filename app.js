9235fc725509eeab535cc17d626f90348eedc3d8
Manan <shahmanan513@gmail.com>
2017-05-10 10:54:41 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// invoking function 
+
+(function(){
+
+	 angular.module("turtleFacts", []); //the array is used when we want to create a module..array is not used when we want to fetch a module like in list.js
+
+	 
+
+}) ();
